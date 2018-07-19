8c14d5cfb08324a34772a2e145fa698f136103af
Li Luo <luoli413@github.com>
2018-07-18 23:40:46 -0400

    rename this feature selection file

--- code/stepwise_inds.py
+++ code/stepwise_inds.py
@@ -9,7 +9,7 @@ warnings.filterwarnings('ignore')
 
 # It's a pipeline to filter stocks quickly based on some features.
 @selecting
-def select_stocks(symbols,context_dict, f_calendar, top_per, bottom_per,instnum = 5):
+def select_stocks(symbols,context_dict, f_calendar, *args,**kwargs):
     # keys = ['west_instnum_FY1','west_herdCV_FY1'] #'herdIndex']
 
     # indexing = f_calendar[-1]
@@ -28,10 +28,10 @@ def select_stocks(symbols,context_dict, f_calendar, top_per, bottom_per,instnum 
 
 def fix_stock_order(order_case):
 
-    def wrapper(test_y,long_position,short_position,*args):
+    def wrapper(test_y,long_position,short_position,*args,**kwargs):
         weight_new = pd.Series(np.zeros(test_y.shape[0]), index=test_y.index)
         flag = True
-        pool_long,pool_short = order_case(test_y,*args)
+        pool_long,pool_short = order_case(test_y,*args,**kwargs)
         if len(pool_long)>0:
             weight_new.loc[pool_long] = long_position / len(pool_long)
             # print(len(pool_long))
@@ -50,18 +50,22 @@ def fix_stock_order(order_case):
 
 #  It's used to order stocks
 @fix_stock_order
-def order_method(test_y,context_dict,cur_date,remove,bottom_thre=1.0,top_thre=0.0,):
+def order_method(test_y,context_dict,cur_date,thre):
 
     pool_short = []
     pool_long = []
     # case 1: only long position
-    pool1 = test_y[test_y > test_y.quantile(q=1 - bottom_thre)]
-    pool2 = test_y[test_y <= test_y.quantile(q=1 - top_thre)]
-    pool_long = pool2.loc[pool1.index].index.values
+    pool1 = test_y[test_y > test_y.quantile(q=1 - thre['long_thre'][1])]
+    pool2 = test_y[test_y <= test_y.quantile(q=1 - thre['long_thre'][0])]
+    if len(list(set(pool1.index.values).intersection(set(pool2.index.values))))>0:
+        temp = pool2.loc[pool1.index]
+        pool_long = temp[temp>0].index.values
     # add short position
-    pool1 = test_y[test_y <= test_y.quantile(q= bottom_thre)]
-    pool2 = test_y[test_y > test_y.quantile(q= top_thre)]
-    pool_short = pool2.loc[pool1.index].index.values
+    pool1 = test_y[test_y <= test_y.quantile(q= thre['short_thre'][1])]
+    pool2 = test_y[test_y > test_y.quantile(q= thre['short_thre'][0])]
+    if len(list(set(pool1.index.values).intersection(set(pool2.index.values)))) > 0:
+        temp = pool2.loc[pool1.index]
+        pool_short = temp[temp<0].index.values
 
     # case ??
     # indicator = test_y.loc[:,~np.isin(test_y.columns,remove)]
@@ -71,6 +75,7 @@ def order_method(test_y,context_dict,cur_date,remove,bottom_thre=1.0,top_thre=0.
 
     return pool_long,pool_short
 
+
 if __name__ == "__main__":
 
     # parameters initialization
@@ -78,23 +83,19 @@ if __name__ == "__main__":
                      'quant_sentiment_si','quant_sentiment_pc','quant_fundamental_pe','quant_fundamental_pb',\
                      'quant_fundamental_pcf','quant_fundamental_div','quant_global_sector','quant_global_country',\
                      'quant_quality_diversification','quant_quality_firm','quant_quality_liquidity']
-    leverage = 0.95
-    long_position = 1.2
-    short_position = 1 - long_position
-    # end_day = -1 # -1 means till today
+
     start_day = '2012-01-01'
-    trading_days = 252.0
-    interest_rate = 0.0
     horizon = 21*1
     freq = 21*1  # rebalance monthly
-    roll = 36 # rolling in x months
+    roll = -1 # rolling in x months
     ben = 'ACWI' # benchmark
     model_name = 'StepWise'
     relative = True
 
     # Back-test initialization
-    context = context(start_day,leverage,long_position,short_position,interest_rate,trading_days,variable_list,)
+    context = context(ben, start_day,variable_list,freq,)
     # Form training set and you just need run once and after that you can comment it until you change
     #  variable list or other parameters.
-    # context.generate_train(horizon, relative, ben, normalize=True)
-    context.integrate_summary(horizon,freq,model_name,select_stocks,roll=roll)
+    context.generate_train(horizon, relative, normalize=True,method='98%shrink')
+    context.feature_selection(horizon,method=model_name,freq=freq,model_name = model_name,\
+                              select_method=select_stocks,roll=roll)
